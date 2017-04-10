b7bd233ea30d369b0e27bf7bc838877ac706a447
xianjingfeng <583872483@qq.com>
2017-04-10 21:20:07 +0800

    ...

--- src/main/java/io/sugo/collect/LogCollector.java
+++ src/main/java/io/sugo/collect/LogCollector.java
@@ -5,7 +5,6 @@ import io.sugo.collect.reader.ReaderFactory;
 import io.sugo.collect.writer.AbstractWriter;
 import io.sugo.collect.writer.WriterFactory;
 
-import java.lang.reflect.Constructor;
 
 /**
  * Created by fengxj on 4/8/17.
--- src/main/java/io/sugo/collect/reader/file/SugoFileFilter.java
+++ src/main/java/io/sugo/collect/reader/file/SugoFileFilter.java
@@ -4,7 +4,6 @@ package io.sugo.collect.reader.file;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.lang.StringUtils;
 
-import javax.swing.filechooser.FileFilter;
 import java.io.File;
 import java.util.regex.Pattern;
 
