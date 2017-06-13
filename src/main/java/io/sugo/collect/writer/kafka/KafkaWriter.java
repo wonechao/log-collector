9636f22a0a32829b972e1c64883cac9fa7eea756
sugo <sugo@appledeMacBook-Pro-3.local>
2017-06-13 15:02:49 +0800

    修改kafka发送方式

--- src/main/java/io/sugo/collect/writer/kafka/KafkaWriter.java
+++ src/main/java/io/sugo/collect/writer/kafka/KafkaWriter.java
@@ -2,17 +2,13 @@ package io.sugo.collect.writer.kafka;
 
 import io.sugo.collect.Configure;
 import io.sugo.collect.writer.AbstractWriter;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 
@@ -43,28 +39,65 @@ public class KafkaWriter extends AbstractWriter {
   @Override
   public boolean write(List<String> messages) {
     long now = 0;
-    if(logger.isDebugEnabled()){
+    if (logger.isDebugEnabled()) {
       now = System.currentTimeMillis();
       logger.debug("send to kafka, message size:" + messages.size());
     }
     List<Future<RecordMetadata>> futures = new ArrayList<>();
+
+    KafkaCallBack callBack = new KafkaCallBack();
     for (String message : messages) {
-      futures.add(producer.send(new ProducerRecord<Integer, String>(this.topic, message)));
+      producer.send(new ProducerRecord<Integer, String>(this.topic, message), callBack);
     }
     producer.flush();
-    if(logger.isDebugEnabled()){
-      long current = System.currentTimeMillis();
-      logger.debug("send to kafka successfully, druration(ms):" + (current - now));
-    }
-    try {
-      for (Future<RecordMetadata> future : futures) {
-        future.get();
+    while (true){
+      if (callBack.getError() > 0){
+        logger.warn("failed to send to kafka ,error:" + callBack.getError());
+        return false;
       }
-    } catch (InterruptedException | ExecutionException e) {
-      logger.debug("", e);
-      return false;
+      if (callBack.getSuccess() == messages.size()){
+        if (logger.isDebugEnabled()) {
+          long current = System.currentTimeMillis();
+          logger.debug("success to send to kafka, druration(ms):" + (current - now));
+        }
+        return true;
+      }
     }
-    return true;
+
+
+//    if (logger.isDebugEnabled()) {
+//      long current = System.currentTimeMillis();
+//      logger.debug("send to kafka successfully, druration(ms):" + (current - now));
+//    }
+//    try {
+//      for (Future<RecordMetadata> future : futures) {
+//        future.get();
+//      }
+//    } catch (InterruptedException | ExecutionException e) {
+//      logger.error("", e);
+//      return false;
+//    }
+//    return true;
+  }
+
+  class KafkaCallBack implements Callback {
+    private int success;
+    private int error;
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+      if (metadata != null) {
+        success ++;
+      } else {
+        error ++;
+        if (logger.isDebugEnabled())
+          logger.error("", exception);
+      }
+    }
+    public int getSuccess(){
+      return success;
+    }
+    public int getError(){
+      return error;
+    }
   }
 
 }
