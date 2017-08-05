9b6dca07618eb23122fc22007550c3484f5c491a
xianjingfeng <583872483@qq.com>
2017-08-05 10:28:53 +0800

    加入文件锁，只允许一个进程运行

--- src/main/java/io/sugo/collect/LogCollector.java
+++ src/main/java/io/sugo/collect/LogCollector.java
@@ -2,13 +2,14 @@ package io.sugo.collect;
 
 import io.sugo.collect.reader.AbstractReader;
 import io.sugo.collect.reader.ReaderFactory;
-import io.sugo.collect.reader.file.DefaultFileReader;
 import io.sugo.collect.writer.AbstractWriter;
 import io.sugo.collect.writer.WriterFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Set;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
 
 /**
@@ -18,6 +19,14 @@ public class LogCollector {
   private static final Logger logger = LoggerFactory.getLogger(LogCollector.class);
   public static void main(String[] args) throws Exception {
     Configure conf = new Configure();
+    String logfile = conf.getProperty(Configure.USER_DIR) + "/.lock";
+    FileChannel channel = new FileOutputStream(logfile, true).getChannel();
+    FileLock lock = channel.tryLock();
+    if(lock == null) {
+      logger.warn("Another process is running, please stop it first!");
+     return;
+    }
+
     AbstractWriter writer = new WriterFactory(conf).createWriter();
     final AbstractReader reader = new ReaderFactory(conf).createReader(writer);
 
