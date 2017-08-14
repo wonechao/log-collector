7f095580c445d3fefa1a5ce093a9ae950612c868
xianjingfeng <583872483@qq.com>
2017-08-14 16:40:44 +0800

    修改HttpUtil,metric发送失败重发

--- src/main/java/io/sugo/collect/util/HttpUtil.java
+++ src/main/java/io/sugo/collect/util/HttpUtil.java
@@ -1,12 +1,9 @@
 package io.sugo.collect.util;
 
-import io.sugo.collect.reader.file.DefaultFileReader;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -14,8 +11,7 @@ import java.io.IOException;
  * Created by fengxj on 7/20/17.
  */
 public class HttpUtil {
-  private static final Logger logger = LoggerFactory.getLogger(HttpUtil.class);
-  public static String post(String url,String data){
+  public static String post(String url,String data) throws IOException{
     HttpClient client = new HttpClient();
     PostMethod postMethod = new PostMethod(url);
     try {
@@ -23,19 +19,16 @@ public class HttpUtil {
       int statusCode = client.executeMethod(postMethod);
 
       if (!(statusCode == HttpStatus.SC_OK || statusCode == HttpStatus.SC_NO_CONTENT)) {
-        logger.warn("Method failed: " + postMethod.getStatusLine());
+        throw new IOException("Method failed: " + postMethod.getStatusLine());
       }
 
       byte[] bytes = postMethod.getResponseBody();
       if (bytes == null)
-        return null;
+        return "";
       return new String(bytes);
-    } catch (IOException  e) {
-      logger.error("", e);
     } finally {
       // Release the connection.
       postMethod.releaseConnection();
     }
-    return null;
   }
 }
