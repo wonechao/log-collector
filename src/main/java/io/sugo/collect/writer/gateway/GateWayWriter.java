5cbbab0a4f93b8de42ab10cdb8cd017b07a25e99
Zack <zackx@foxmail.com>
2017-08-14 15:00:03 +0800

    改变GateWayWriter发送的body内容格式 把JSON改为以"\n"分割的字符串

--- src/main/java/io/sugo/collect/writer/gateway/GateWayWriter.java
+++ src/main/java/io/sugo/collect/writer/gateway/GateWayWriter.java
@@ -36,8 +36,8 @@ public class GateWayWriter extends AbstractWriter {
 
     Gson gson = new Gson();
     String messagesJSON = gson.toJson(messages);
-
-    return flush(messagesJSON);
+    String result = messagesJSON.substring(1, messagesJSON.length() - 2).replace("\",\"", "\"\n\"");
+    return flush(result);
   }
 
   private boolean flush(String jsonString) {
