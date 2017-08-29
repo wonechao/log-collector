681d9f7a71bc0c2cb694445f7b7e11c00bd17f02
lzackx <zackx@foxmail.com>
2017-08-29 10:16:33 +0800

    Fix new format

--- src/main/java/io/sugo/collect/parser/BBKHybridParser.java
+++ src/main/java/io/sugo/collect/parser/BBKHybridParser.java
@@ -1,12 +1,10 @@
 package io.sugo.collect.parser;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import io.sugo.collect.Configure;
 import io.sugo.collect.util.AESUtil;
 import io.sugo.collect.util.RSAUtil;
-import io.sugo.grok.api.Grok;
-import io.sugo.grok.api.Match;
-import io.sugo.grok.api.exception.GrokException;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,34 +13,13 @@ import java.util.Map;
 public class BBKHybridParser extends GrokParser {
 
     private final Logger logger = LoggerFactory.getLogger(BBKHybridParser.class);
-    public static final String FILE_READER_GROK_PATTERNS_PATH = "file.reader.grok.patterns.path";
-    public static final String FILE_READER_DECRYPTION_PRIVATE = "file.reader.decryption.private";
-    public static final String FILE_READER_GROK_EXPR = "file.reader.grok.expr";
+    private static final String HTTP_BASE_REQUEST_PARAM = "http_Base_Request_Param";
     private static final String EXCEPTION_KEY = "exception";
-    private Grok grok;
+    public static final String FILE_READER_DECRYPTION_PRIVATE = "file.reader.decryption.private";
     private String privateKey;
 
     public BBKHybridParser(Configure conf) {
         super(conf);
-        try {
-            String patternPath = conf.getProperty(FILE_READER_GROK_PATTERNS_PATH);
-            if (StringUtils.isBlank(patternPath)){
-                patternPath = conf.getProperty(Configure.USER_DIR) + "conf/patterns";
-            }
-            if (!patternPath.startsWith("/"))
-                patternPath = conf.getProperty(Configure.USER_DIR) + "/" + patternPath;
-            logger.info("final patternPath:" + patternPath);
-            grok = Grok.create(patternPath);
-            String grokExpr = conf.getProperty(FILE_READER_GROK_EXPR);
-            if (StringUtils.isBlank(grokExpr)){
-                logger.error(FILE_READER_GROK_EXPR + "must be set!");
-                System.exit(1);
-            }
-            grok.compile(grokExpr);
-        } catch (GrokException e) {
-            logger.error("", e);
-            System.exit(1);
-        }
         this.privateKey = conf.getProperty(FILE_READER_DECRYPTION_PRIVATE, "");
     }
 
@@ -50,21 +27,30 @@ public class BBKHybridParser extends GrokParser {
     public Map<String, Object> parse(String line) throws Exception {
 
         Map<String, Object> map = super.parse(line);
+        if (map.isEmpty()) {
+            throw new Exception("Parse failed: " + map);
+        }
         String httpEncrypted = (String) map.get("http_encrypted");
         if (httpEncrypted.equals("encrypted")) {
-            return encryptedParse(line);
+            return encryptedParse(map);
         } else {
-            return unencryptedParse(line);
+            return unencryptedParse(map);
         }
     }
 
-    Map<String, Object> unencryptedParse(String line) throws Exception {
+    Map<String, Object> unencryptedParse(Map<String, Object> map) throws Exception {
 
+        Gson gson = new Gson();
         //参考nginx日志模块ngx_http_log_escape方法
         //" \ del  会被转为\x22 \x5C \x7F
         //https://github.com/nginx/nginx/blob/9ad18e43ac2c9956399018cbb998337943988333/src/http/modules/ngx_http_log_module.c
-        line = line.replace("\\x5C", "\\").replace("\\x22", "\"");
-        Map<String, Object> map = super.parse(line);
+        String httpBaseRequestParam = ((String) map.get(HTTP_BASE_REQUEST_PARAM)).replace("\\x5C", "\\").replace("\\x22", "\"");
+        Map<String, String> httpBaseRequestParamMap = gson.fromJson(httpBaseRequestParam, new TypeToken<Map<String, String>>(){}.getType());
+        for (String key : httpBaseRequestParamMap.keySet()) {
+            map.put(key, httpBaseRequestParamMap.get(key));
+        }
+        map.remove(HTTP_BASE_REQUEST_PARAM);
+
         if (map.containsKey(EXCEPTION_KEY)) {
             Object exVal = map.get(EXCEPTION_KEY);
             map.put("exception_size", exVal.toString().length());
@@ -74,14 +60,25 @@ public class BBKHybridParser extends GrokParser {
         return map;
     }
 
-    Map<String, Object> encryptedParse(String line) throws Exception {
-        Match gm = this.grok.match(line);
-        gm.captures();
-        Map<String, Object> map = gm.toMap();
-        String key = "";
-        key = RSAUtil.priDecrypt(map.get("http_eebbk_key").toString(), this.privateKey);
-        String result = AESUtil.decryptAES(map.get("http_Base_Request_Param").toString(), key);
-        map.put("http_Base_Request_Param", result);
+    Map<String, Object> encryptedParse(Map<String, Object> map) throws Exception {
+
+        String pk = "";
+        pk = RSAUtil.priDecrypt(map.get("http_eebbk_key").toString(), this.privateKey);
+        String result = AESUtil.decryptAES(map.get(HTTP_BASE_REQUEST_PARAM).toString(), pk);
+        Gson gson = new Gson();
+        Map<String, String> httpBaseRequestParamMap = gson.fromJson(result, new TypeToken<Map<String, String>>(){}.getType());
+        for (String key : httpBaseRequestParamMap.keySet()) {
+            map.put(key, httpBaseRequestParamMap.get(key));
+        }
+        map.remove(HTTP_BASE_REQUEST_PARAM);
+
+        if (map.containsKey(EXCEPTION_KEY)) {
+            Object exVal = map.get(EXCEPTION_KEY);
+            map.put("exception_size", exVal.toString().length());
+            map.put(EXCEPTION_KEY, exVal);
+            map.remove(EXCEPTION_KEY);
+        }
+
         return map;
     }
 
