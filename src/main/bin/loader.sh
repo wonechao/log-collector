b0c38f2b4f2831ef01381c5267ab0b9ff5a5af1c
janpychou <janpychou@qq.com>
2017-09-14 21:34:11 +0800

    load kafka data to file

---
+++ src/main/bin/loader.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd `dirname $0`
+cd ..
+project_dir=`pwd`
+JAVA=$JAVA_HOME'/bin/java'
+if [ -z $JAVA_HOME ]; then
+ JAVA='java'
+fi
+conf_file=${project_dir}'/conf/collect.properties'
+
+pidfile=${project_dir}/collector.pid
+if [ -f $pidfile ];then
+   ps -ef |grep `cat ${pidfile}` |grep -v 'grep'
+   if [ $? -ne 0 ];then
+      echo 'Another process is running, please stop it first!'
+      exit 1
+   fi
+fi
+nohup $JAVA -Dcollect.properties=$conf_file -cp  "${project_dir}/lib/*:${project_dir}/conf/"  io.sugo.collect.kafka.loader.KafkaDataLoader > /dev/null &
+if [ $? -eq 0 ];then
+  echo $! > ${project_dir}/collector.pid
+  echo 'PID:'$!' is started!'
+fi
